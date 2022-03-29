fad825e44a995669d3b9412d913bf0d25d60e027
Elve Johansson <elve2002@live.se>
2022-03-29 17:26:38 +0200

    feat: colors & prompt
    - Added chalk and chalk-animations to make terminal prettier.
    - Added enquirer to handle user input and created basic prompt.

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 #!/usr/bin/env node
 
-console.log("app.js");
+import chalkAnimation from "chalk-animation";
+import inquirer from "inquirer";
+
+const sleep = (ms = 2000) => new Promise(resolve => setTimeout(resolve, ms));
+
+async function header() {
+	const title = chalkAnimation.rainbow("\nWelcome to NEC - the Node Ecosystem CLI!\n");
+
+	await sleep();
+	title.stop();
+	console.clear();
+
+	inquirer
+		.prompt({
+			type: "list",
+			name: "entry",
+			message: "What do you want to do?",
+			choices: [
+				"Create project.",
+				"Modify existing project.",
+				"Exit."
+			]
+		})
+		.then(answers => {
+			console.log(answers);
+		})
+		.catch(err => {
+			console.error(err);
+		});
+}
+
+console.clear();
+await header();
