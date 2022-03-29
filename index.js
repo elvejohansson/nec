553cba1b537b148099c09c9494593807e55cf314
Elve Johansson <elve2002@live.se>
2022-03-29 18:52:22 +0200

    feat: exit function
    - Added function for checking user entry and exits application if user chose it.

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ import inquirer from "inquirer";
 
 const sleep = (ms = 2000) => new Promise(resolve => setTimeout(resolve, ms));
 
+/**
+ * Function for checking the chosen entry and exiting the application if the user selected "Exit.".
+ * @param {string} entry - The entry that the user selected.
+ * @returns {bool} false if the user doesn't want to exit, otherwise expect the process to exit.
+ */
+async function checkExit(entry) {
+	if (entry !== "Exit") {
+		return false;
+	}
+	
+	console.log("\nExiting...");
+	sleep(250)
+		.then(() => process.exit(1))
+		.catch(err => console.error(err));
+	return true;
+}
+
 async function header() {
 	const title = chalkAnimation.rainbow("\nWelcome to NEC - the Node Ecosystem CLI!\n");
 
@@ -18,13 +35,13 @@ async function header() {
 			name: "entry",
 			message: "What do you want to do?",
 			choices: [
-				"Create project.",
-				"Modify existing project.",
-				"Exit."
+				"Create project",
+				"Modify existing project",
+				"Exit"
 			]
 		})
 		.then(answers => {
-			console.log(answers);
+			checkExit(answers.entry);
 		})
 		.catch(err => {
 			console.error(err);
