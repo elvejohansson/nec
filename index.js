d0f107a9c8e3261b9eb8672f239c5ee8987ef299
Elve Johansson <elve2002@live.se>
2022-03-31 11:17:06 +0200

    refactor: moved utility function to own directory
    - Moved sleep and checkExit function to new utils directory for better code readability.

--- index.js
+++ index.js
@@ -14,32 +14,11 @@ import chalk from "chalk";
 import chalkAnimation from "chalk-animation";
 import inquirer from "inquirer";
 
+import {
+	sleep,
+	checkExit
+} from "./utils/index.js";
 
-/**
- * Function for sleeping the application for a certain amount of time (in milliseconds).
- * Default is 2000 milliseconds.
- * @param {number} milliseconds - The number of milliseconds to sleep. 
- * @returns {Promise} A promise that resolves after the specified number of milliseconds.
- */
-function sleep(ms = 2000) {
-	return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-/**
- * Function for checking the chosen entry and exiting the application if the user selected "Exit.".
- * @param {string} entry - The entry that the user selected.
- * @returns {bool} false if the user doesn't want to exit, otherwise expect the process to exit.
- */
-async function checkExit(entry) {
-	if (entry !== "Exit") {
-		return false;
-	}
-
-	chalkAnimation.rainbow("\nExiting...\n");
-	await sleep(1000);
-	console.clear();
-	process.exit(1);
-}
 
 /**
  * Function for creating a new project.
---
+++ utils/index.js
@@ -0,0 +1,33 @@
+import chalkAnimation from "chalk-animation";
+
+export {
+	sleep,
+	checkExit
+};
+
+
+/**
+ * Function for sleeping the application for a certain amount of time (in milliseconds).
+ * Default is 2000 milliseconds.
+ * @param {number} milliseconds - The number of milliseconds to sleep. 
+ * @returns {Promise} A promise that resolves after the specified number of milliseconds.
+ */
+function sleep(ms = 2000) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+ * Function for checking the chosen entry and exiting the application if the user selected "Exit.".
+ * @param {string} entry - The entry that the user selected.
+ * @returns {bool} false if the user doesn't want to exit, otherwise expect the process to exit.
+ */
+ async function checkExit(entry) {
+	if (entry !== "Exit") {
+		return false;
+	}
+
+	chalkAnimation.rainbow("\nExiting...\n");
+	await sleep(1000);
+	console.clear();
+	process.exit(1);
+}
